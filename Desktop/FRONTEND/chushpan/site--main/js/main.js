5c53d832e38382831618cdef6a1702994c00309b
kreysm <kreismann.artem02@mail.ru>
2025-08-12 05:37:32 +0300

    add roulette

--- Desktop/FRONTEND/chushpan/site--main/js/main.js
+++ Desktop/FRONTEND/chushpan/site--main/js/main.js
@@ -1,12 +1,14 @@
 const audio = document.getElementById('myAudio');
 const playButton = document.getElementById('playButton');
+const playIcon = document.getElementById('playIcon');
 
 playButton.addEventListener('click', function(event) {
-  event.preventDefault();  
-  
+  event.preventDefault();
   if (audio.paused) {
     audio.play();
+    playIcon.src = 'images/pause.png';  // Путь к картинке для паузы
   } else {
     audio.pause();
+    playIcon.src = 'images/play.png';  // Путь к картинке для воспроизведения
   }
 });
