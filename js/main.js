ede5c7d3876b970f07d90133653ef8d9b679cb46
kreysm <kreismann.artem02@mail.ru>
2025-08-14 13:24:13 +0300

    update

--- js/main.js
+++ js/main.js
@@ -6,9 +6,22 @@ playButton.addEventListener('click', function(event) {
   event.preventDefault();
   if (audio.paused) {
     audio.play();
-    playIcon.src = 'images/pause.png';  // Путь к картинке для паузы
+    playIcon.src = 'images/pause.png';  
   } else {
     audio.pause();
-    playIcon.src = 'images/play.png';  // Путь к картинке для воспроизведения
+    playIcon.src = 'images/play.png';  
+  }
+});
+
+$('.burger, .overlay').on('click', function(e) {
+  e.preventDefault();
+
+  if ($('.header__top').hasClass('header__top--open')) {
+    $('.header__top').removeClass('header__top--open');
+    $('.overlay').removeClass('overlay--show');
+  } else {
+
+    $('.header__top').addClass('header__top--open');
+    $('.overlay').addClass('overlay--show');
   }
 });
